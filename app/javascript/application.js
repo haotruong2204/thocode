a5be8097726751d9b0af5cd626ea35e252eb4c5b
Hao Truong <haotv03@gmail.com>
2023-10-27 19:48:38 +0700

    config js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
+import "jquery";
 import "controllers";
+
+$(document).ready(function () {
+  // switch mode
+  $(".js-show-box").on("click", function (e) {
+    $(".js-the-box").toggle("hidden");
+  });
+});
