aac40a5b043721cea1b3a194f9de0d337faee223
Hao Truong <haotv03@gmail.com>
2024-04-04 16:57:56 +0700

    fix ip

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,4 +4,4 @@ set :rack_env, :staging
 set :branch, 'main'
 set :deploy_to, '/var/www/thocode'
 
-server "13.250.244.161", user: "ubuntu", roles: %w(web app db)
+server "52.221.3.10", user: "ubuntu", roles: %w(web app db)
